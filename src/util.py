7f78821ae916be59ad29dbfe350400f5f1442012
SquareWheelBike <colefuerth@gmail.com>
2022-04-20 18:40:06 -0400

    added conversion utils

--- src/util.py
+++ src/util.py
@@ -52,3 +52,22 @@ def setToDeterministicTuple(s):
 
 def removeItem(toRemove, tpl):
     return tuple(filter(lambda x: x != toRemove, tpl))
+
+########################################################################
+# UTILITIES FOR TF DATA FORMATS
+########################################################################
+
+def transform_data(issacinput:list):
+    # convert issac's data format into something that can be fed to a dataframe
+    out = {col : [] for col in issacinput[0].keys()}
+    for d in issacinput:
+        for k,v in d.items():
+            out[k].append(v)
+    return out
+
+def data_str_to_int(data:dict):
+    # takes a dict from transform_data and converts all strings to ints
+    m = {i:o for o,i in enumerate('abcdefghijklmnopqrstuvwxyz?')}
+    for k,v in data.items():
+        data[k] = [m[x] for x in v]
+    return data
