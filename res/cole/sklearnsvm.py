f9082686ec73b7f9e1c81ed9a311cbc0d0a5222d
SquareWheelBike <colefuerth@gmail.com>
2022-04-25 22:33:49 -0400

    cleanup

--- res/cole/sklearnsvm.py
+++ res/cole/sklearnsvm.py
@@ -1,30 +1,12 @@
-# %% [markdown]
-# # there is no stopping the cole train
-#
-# This exists so I can mess around with tf.keras until it successfully trains on the dataset we are using.
-
-# %% [markdown]
-# use tf.keras to train on the dataset found under ../data
-
 # %%
 # import numpy as np
 from progress.bar import IncrementalBar
 from concurrent.futures import ThreadPoolExecutor
 from os import cpu_count
 import pandas as pd
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras import layers
-# from tensorflow.keras.layers.experimental import RandomFourierFeatures
 from sklearn import svm
-# %% [markdown]
-# import the dataset
-#
-# - `../data/attributes.dat` contains the `CSV_HEADER`
-# - `../data/mushrooms.short.dat` contains csv entries
 
 # %%
-
 
 def loadData(dataPath, attributePath):
     # read the attribute name mapping so we can zip it later
@@ -77,37 +59,15 @@ def issac_to_dfdict(data):
 CSV_HEADER, data = loadData(
     '../../data/mushrooms.dat', '../../data/attributes.dat')
 
-# data = pd.DataFrame(data)
-# data['classification'] = data['classification'].apply(lambda x: {'e': 1, 'p': 0}[x])
-# data = data.applymap(lambda x: ord(x) if isinstance(x, str) else x)
 
 data = pd.DataFrame(issac_to_dfdict(data))
 
 data = data.apply(lambda s: s.astype('float32'))
 
 # %%
-
-# CSV_HEADER = open('../../data/attributes.dat').readline().strip().split(',')
-# data = pd.read_csv('../../data/mushrooms.dat', header=None, names=CSV_HEADER)
-
-
-# convert all strings to integers
-# convert classifications to 1 and 0 (binary for accuracy metric later)
-# data['classification'] = data['classification'].apply(
-#     lambda x: {'e':1, 'p':0}[x])
-# data = data.applymap(lambda s: {i: o for o, i in enumerate(
-#     'abcdefghijklmnopqrstuvwxyz?')}[s] if type(s) == str else s)
-
-# %%
 def test(train_data, test_data):
     """train, test, return accuracy"""
-
-    # print(f"Train dataset shape: {train_data.shape}")
-    # print(f"Test dataset shape: {test_data.shape}")
-
-    # build the model
-
-    # one versus one decision shape
+    
     clf = svm.SVC(kernel='linear', decision_function_shape='ovo')
     # clf = svm.LinearSVC()
 
@@ -115,18 +75,6 @@ def test(train_data, test_data):
     (x_train, y_train), (x_test, y_test) = (train_data.drop(columns=[
         'classification']), train_data['classification']), (test_data.drop(columns=['classification']), test_data['classification'])
 
-    # convert all values to float32
-    # x_train = x_train.apply(lambda s: s.astype('float32'))
-    # x_test = x_test.apply(lambda s: s.astype('float32'))
-
-    # # print shapes
-    # print(f"x_train shape: {x_train.shape}")
-    # print(f"y_train shape: {y_train.shape}")
-    # print(f"x_test shape: {x_test.shape}")
-    # print(f"y_test shape: {y_test.shape}")
-
-    # train the model
-    # print(y_train)
     clf.fit(x_train, y_train)
 
     # check accuracy
@@ -170,10 +118,7 @@ with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
         executor.submit(test, block, test_data)
         for block in train_blocks
     ]
-# # progress bar wrapper lol
-# bar = IncrementalBar(f"{k}-Fold Cross-Validation")
-# wrappedFutures = bar.iter(threads)
-# return the average from the evaluation tasks
+    
 print(f'{k}-Fold Cross-Validation Result: {sum(task.result() for task in threads) / len(threads)}')
 
 # %%
